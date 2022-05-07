8296c96addc20248b93c24db4c1542d885c73dfb
Andy Almand-Hunter <ahunter@stitchfix.com>
2022-05-06 21:13:26 -0400

    WIP 1a and 1b

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -2,11 +2,25 @@ module Homework.Week01.Assignment where
 
 -- #1a
 toDigits :: Integer -> [Integer]
-toDigits = undefined
+
+-- toDigits n
+--   | n < 1     = []
+--   | n < 10    = [n]
+--   | otherwise = toDigits (div n 10) ++ [mod n 10]
+
+toDigits n = go n []
+  where go m acc
+          | m < 1     = []
+          | m < 10    = m : acc
+          | otherwise = go (div m 10) (mod m 10 : acc)
+-- ^^ possible to express as a fold?
 
 -- #1b
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev = undefined
+toDigitsRev n
+  | n < 1     = []
+  | n < 10    = [n]
+  | otherwise = mod n 10 : toDigitsRev (div n 10)
 
 -- #2
 doubleEveryOther :: [Integer] -> [Integer]
