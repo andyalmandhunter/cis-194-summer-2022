1a7cfd6c60702b05363f4a83c001286d6f0356b6
Andy Almand-Hunter <ahunter@stitchfix.com>
2022-05-12 10:20:35 -0400

    Simplify 2

--- src/Homework/Week01/Assignment.hs
+++ src/Homework/Week01/Assignment.hs
@@ -17,16 +17,12 @@ toDigitsRev n
   | otherwise = mod n 10 : toDigitsRev (div n 10)
 
 -- #2
-doubleEveryOtherRev :: [Integer] -> [Integer]
-doubleEveryOtherRev []       = []
-doubleEveryOtherRev [n]      = [n]
-doubleEveryOtherRev (n:m:ns) = n : (2*m) : doubleEveryOtherRev ns
-
-rev :: [Integer] -> [Integer]
-rev = foldl (\acc n -> n:acc) []
-
 doubleEveryOther :: [Integer] -> [Integer]
-doubleEveryOther = rev . doubleEveryOtherRev . rev
+doubleEveryOther =
+  let go []       = []
+      go [n]      = [n]
+      go (n:m:ns) = n : (2*m) : go ns
+  in reverse . go . reverse
 
 -- #3
 toDigitsList :: [Integer] -> [Integer]
