add8c447ee9e6bb9b56eaab4d2b25fc9f3dc547b
Andy Almand-Hunter <ahunter@stitchfix.com>
2022-05-15 22:59:08 -0400

    Cleanup

--- src/Homework/Week02/Assignment.hs
+++ src/Homework/Week02/Assignment.hs
@@ -16,10 +16,10 @@ import Homework.Week02.Log
 -- #1a
 parseMessage :: String -> LogMessage
 parseMessage =
-  let go ("I":t:m) = LogMessage Info (read t :: Int) (unwords m)
-      go ("W":t:m) = LogMessage Warning (read t :: Int) (unwords m)
+  let go ("I":t:m)   = LogMessage Info (read t :: Int) (unwords m)
+      go ("W":t:m)   = LogMessage Warning (read t :: Int) (unwords m)
       go ("E":s:t:m) = LogMessage (Error (read s :: Int)) (read t :: Int) (unwords m)
-      go m = Unknown (unwords m)
+      go m           = Unknown (unwords m)
   in go . words
 
 -- #1b
@@ -29,7 +29,7 @@ parse = (map parseMessage) . lines
 -- #2
 insert :: LogMessage -> MessageTree -> MessageTree
 insert (Unknown _) mt = mt
-insert l Leaf = Node Leaf l Leaf
+insert l Leaf         = Node Leaf l Leaf
 insert l@(LogMessage _ t _) (Node a ll@(LogMessage _ tt _) b)
   | t < tt    = Node (insert l a) ll b
   | otherwise = Node a ll (insert l b)
@@ -40,7 +40,7 @@ build = foldl (flip insert) Leaf
 
 -- #4
 inOrder :: MessageTree -> [LogMessage]
-inOrder Leaf = []
+inOrder Leaf         = []
 inOrder (Node a l b) = (inOrder a) ++ [l] ++ (inOrder b)
 
 -- #5
