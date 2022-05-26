19c41aab60573d99ddb327daaccab51044954d58
Andy Almand-Hunter <ahunter@stitchfix.com>
2022-05-26 00:19:31 -0400

    Initial solutions

--- src/Homework/Week03/Assignment.hs
+++ src/Homework/Week03/Assignment.hs
@@ -5,13 +5,42 @@ module Homework.Week03.Assignment (
 ) where
 
 -- #1
+skip :: Int -> [a] -> [a]
+skip n =
+  let f [] = []
+      f (b:bs) = b : (f (drop (n-1) bs))
+  in f . (drop (n-1))
+
 skips :: [a] -> [[a]]
-skips = undefined
+skips as = map (flip skip as) [1..(length as)]
 
 -- #2
 localMaxima :: [Integer] -> [Integer]
-localMaxima = undefined
+localMaxima =
+  let f ms (a:b:c:ns)
+        | b > a && b > c = f (b:ms) (b:c:ns)
+        | otherwise      = f ms (b:c:ns)
+      f ms _ = ms
+  in reverse . (f [])
 
 -- #3
+count :: [Integer] -> Integer -> Int
+count ns n = (length . filter (==n)) ns
+
+row :: Int -> [Integer] -> [Bool]
+row n ms = map ((>=n) . count ms) [0..9]
+
+rows :: [Integer] -> [[Bool]]
+rows ns = takeWhile (any id) $ map ((flip row) ns) [1..]
+
+histRow :: [Bool] -> String
+histRow =
+  let f True  = '*'
+      f False = ' '
+  in map f
+
+hist :: [Integer] -> String
+hist = unlines . map histRow . reverse . rows
+
 histogram :: [Integer] -> String
-histogram = undefined
+histogram ns = (hist ns) ++ "==========\n0123456789\n"
